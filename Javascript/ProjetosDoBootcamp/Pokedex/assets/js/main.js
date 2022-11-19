55db9fd2beb2c473a0f91eed5626ea754e1a009b
DidoReis <djdidoreis@gmail.com>
2022-11-19 15:50:21 -0300

    Finalização do projeto

--- Javascript/ProjetosDoBootcamp/Pokedex/assets/js/main.js
+++ Javascript/ProjetosDoBootcamp/Pokedex/assets/js/main.js
@@ -1,25 +1,48 @@
-function convertPokemonLi(pokemon) {
-  return `
-  <ol class="pokemons">
-  <li class="pokemon">
-    <span class="number">#001</span>
+const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
+const limit = 10;
+let offset = 0;
+
+const maxRecords = 151;
+
+function loadPokemonItens(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons
+      .map(
+        (pokemon) => `
+    <li class="pokemon ${pokemon.type}">
+    <span class="number">#${pokemon.number}</span>
     <span class="name">${pokemon.name}</span>
     <div class="details">
       <ol class="types">
-        <li class="type">Grass</li>
-        <li class="type">Poison</li>
+        ${pokemon.types
+          .map((type) => `<li class="type ${type}">${type}</li>`)
+          .join("")}
       </ol>
       <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+        src="${pokemon.photo}"
         alt="${pokemon.name}"
       />
     </div>
   </li>
-</ol>
-  `;
-}
 
-const pokemonList = document.getElementById("pokemonList");
-pokeApi.getPokemons().then((pokemons = []) => {
-  pokemonList.innerHTML += pokemons.map(convertPokemonLi).join("");
+  `
+      )
+      .join("");
+    pokemonList.innerHTML += newHtml;
+  });
+}
+loadPokemonItens(offset, limit);
+loadMoreButton.addEventListener("click", () => {
+  offset += limit;
+
+  const qtdRecordNextPage = offset + limit;
+
+  if (qtdRecordNextPage >= maxRecords) {
+    const newLimit = maxRecords - offset;
+    loadPokemonItens(offset, limit);
+    loadMoreButton.parentElement.removeChild(loadMoreButton);
+  } else {
+    loadPokemonItens(offset, limit);
+  }
 });
